0d8cfa888d901e23ae1cc063f7a282ed51eee4b5
yankang <yankang@jd.com>
2019-08-27 20:28:47 +0800

    test

---
+++ packages/dt/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dt/dt.dart' as dt;
+
+main(List<String> arguments) {
+  print('Hello world: ${dt.calculate()}!');
+}
---
+++ packages/dt/test/dt_test.dart
@@ -0,0 +1,8 @@
+import 'package:dt/dt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
